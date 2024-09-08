e7586f2ba1cb2ccc846842586842398e9f78b71a
reymart-vi <110107868+reymart-vi@users.noreply.github.com>
2024-09-09 01:29:27 +0800

    test 14

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,30 @@
-const express = require('express');
-const app = express();
 
+const express = require('express');
+const app  = express()
+require('dotenv').config()
+const path = require("path");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+
+app.set("view engine", "ejs",);
+app.set("views", 'views');
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
 //////////////////////////////////////////////////////////////////////  HOME
 const home = require('../routes/home')
-app.use('/', home)
+app.use('/home', home)
 //////////////////////////////////////////////////////////////////////  BITLY
 const bitly = require('../routes/bitlyAPI')
 app.use('/bitly', bitly)
 
-module.exports = app;
+const port = process.env.PORT;
+app.listen(port, ()=>{
+        // console.log('Server is now live at --- %chttp://localhost:'+port, 'background: #222; color: #bada55'); //chrome
+        console.log('Server is now live at --- \u001b[1;32m http://localhost:'+port)
+})
