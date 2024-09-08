43780395760c86a05261c2efd86355669611d147
reymart-vi <110107868+reymart-vi@users.noreply.github.com>
2024-09-09 00:54:48 +0800

    test 10

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+
+const path = require("path");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.set("view engine", "ejs",);
+app.engine('ejs', require('ejs').__express);
+
+app.set("views", [path.join(__dirname, 'views')]);
+app.use(express.static(__dirname + "/public"));
 //////////////////////////////////////////////////////////////////////  HOME
 app.get('/', (req, res) => {
         res.render("home");
